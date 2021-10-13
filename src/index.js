acfbdc08057e544c657b9a73aa6db8782d5c9b8c
JenyferMarquez <favimarqez3@gmail.com>
2021-10-13 18:12:09 -0500

    formulario en html

--- src/index.js
+++ src/index.js
@@ -1,6 +1,126 @@
 import validator from './validator.js';
 
+// ----- boton ver mas ------//
 
+document.getElementById("verMas").addEventListener("click",()=>{    
+    document.getElementById("principal").style.display= "none" ;
+    document.getElementById("productos").style.display= "block" ;
+    document.getElementById("aceiteRosas").style.display= "none" ;
+    document.getElementById("aceiteMenta").style.display= "none ";
+    document.getElementById("aceiteCanela").style.display= "none" ;
+
+} 
+)   
+// ----- boton aceite de Rosas ------//
+document.getElementById("aceitRosas").addEventListener("click",()=>{ 
+    
+        document.getElementById("principal").style.display= "none" ;
+        document.getElementById("productos").style.display= "none" ;
+        document.getElementById("aceiteRosas").style.display= "block" ;
+        document.getElementById("aceiteMenta").style.display= "none ";
+        document.getElementById("aceiteCanela").style.display= "none" ;
+
+}   
+)
+
+// ----- boton aceite de Menta ------//
+document.getElementById("aceiMenta").addEventListener("click",()=>{ 
+    
+    document.getElementById("principal").style.display= "none" ;
+    document.getElementById("productos").style.display= "none" ;
+    document.getElementById("aceiteRosas").style.display= "none" ;
+    document.getElementById("aceiteMenta").style.display= "block ";
+    document.getElementById("aceiteCanela").style.display= "none" ;
+
+}   
+)
+// ----- boton aceite de Canela ------//
+document.getElementById("aceiCanela").addEventListener("click",()=>{ 
+    
+    document.getElementById("principal").style.display= "none" ;
+    document.getElementById("productos").style.display= "none" ;
+    document.getElementById("aceiteRosas").style.display= "none" ;
+    document.getElementById("aceiteMenta").style.display= "none ";
+    document.getElementById("aceiteCanela").style.display= "block" ;
+
+}   
+)
+// ----- boton comprar ------//   
+document.getElementById("comprarUno").addEventListener("click",()=>{ 
+    
+    document.getElementById("principal").style.display= "none" ;
+    document.getElementById("productos").style.display= "none" ;
+    document.getElementById("aceiteMenta").style.display= "none" ;
+    document.getElementById("formularioDePago").style.display= "block";
+    document.getElementById("aceiteCanela").style.display= "none" ;
+    document.getElementById("aceiteRosas").style.display= "none";
+}   
+)
+
+
+document.getElementById("volverUno").addEventListener("click",()=>{ 
+    
+    document.getElementById("principal").style.display= "block" ;
+    document.getElementById("productos").style.display= "none" ;
+    document.getElementById("aceiteMenta").style.display= "none" ;
+    document.getElementById("formularioDePago").style.display= "none";
+    document.getElementById("aceiteCanela").style.display= "none" ;
+    document.getElementById("aceiteRosas").style.display= "none"
+}   
+)
+
+document.getElementById("comprarDos").addEventListener("click",()=>{ 
+    
+    document.getElementById("principal").style.display= "none" ;
+    document.getElementById("productos").style.display= "none" ;
+    document.getElementById("aceiteMenta").style.display= "none" ;
+    document.getElementById("formularioDePago").style.display= "block";
+    document.getElementById("aceiteRosas").style.display= "none" ;
+
+}   
+)
+
+document.getElementById("volverDos").addEventListener("click",()=>{ 
+    
+    document.getElementById("principal").style.display= "block" ;
+    document.getElementById("productos").style.display= "none" ;
+    document.getElementById("aceiteMenta").style.display= "none" ;
+    document.getElementById("formularioDePago").style.display= "none";
+    document.getElementById("aceiteCanela").style.display= "none" ;
+    document.getElementById("aceiteRosas").style.display= "none"
+}   
+)
+
+      
+
+const confirmacionTarjeta = () => {
+	
+
+    let numeroTarjeta = document.getElementById("userNum").value // Valor numero de tarjeta 
+  
+  
+    if (numeroTarjeta.length < 16 || numeroTarjeta === 0) { //alert número erróneo 
+        alert("Ingresa un número de tarjeta válido.");
+        return
+    }
+    else {
+        let validacion = validator.isValid(numeroTarjeta); //retorna verdadero o falso
+  
+  
+        if (validacion == false) {  //tiene el resultado de validator == compara === valor y tipo de datos el mismo 
+            alert("Revisa tus datos nuevamente.");
+            return
+  
+  
+        } else {
+            alert("Ingresado correctamente " + validator.maskify(numeroTarjeta));
+            return
+        }
+    }
+  }
+  
+  let prueba = document.getElementById("submitBtn")
+prueba.addEventListener("click", confirmacionTarjeta)
 
  
 
--- src/validator.js
+++ src/validator.js
@@ -1,4 +1,71 @@
 
+// números tarjeta = true or false 
 
+const validator = { // Objeto, isValid, Maskify es un método
+    isValid:function (numeroTarjeta) {
+      //Se evita un string vació con null 
+      if (numeroTarjeta == "" || numeroTarjeta == null) {
+        return false;
+      }
+      //se parcea para evitar sólo núneros 0
+      if (parseInt(numeroTarjeta ) == 0) {
+        return false;
+      }
+  
+  
+      let newArray = numeroTarjeta .split("").map(Number).reverse();
+  
+  
+      //Tomar pares para multiplicarlos
+      for (let i = 0; i < newArray.length; i++) {
+        // divido en 2 no es igual a 0
+        if (i % 2 != 0) {
+          newArray[i] = (newArray[i] * 2)
+        }
+        // > 9 debo sumar los dígitos
+        if (newArray[i] > 9) {
+          let newValues = newArray[i].toString().split("").map(Number);
+  
+  
+          newArray[i] = newValues[0] + newValues[1];
+        }
+      }
+      //Sumar todos los indices y si termina en cero la tarjeta es válida
+      let newValuesOne = 0;
+      for (let i = 0; i < newArray.length; i++) {
+        newValuesOne = newArray[i] + newValuesOne;
+      }
+  
+  
+      return newValuesOne % 10 == 0 ? true : false;
+    },
+  
+  
+    //Maskify = método que oculta los números a excepción de los últimos cuatro
+  
+  
+    maskify: (numeroTarjeta ) => { //elemento flecha
+  
+  
+      //long es el largo de cardNumber - 4 posiciones
+      let long = numeroTarjeta.length - 4;
+      //últimos 4 carácteres 
+      let lastFour = numeroTarjeta.substring(numeroTarjeta.length - 4)
+      let result = "";
+      //for recorre el arreglo exceptuando las últimas 4 posiciones
+      for (let mask = 1; mask <= long; mask++) {
+        //se suman los números finales + los que están ocultos 
+        result += "#";
+      }
+  
+  
+      //Sumo a mi arreglo final los Strings
+  
+  
+      return result + lastFour;
+    }
+  }
+  ;
+  
   
   export default validator;
